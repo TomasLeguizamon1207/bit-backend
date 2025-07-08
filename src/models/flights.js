574283f8368f32ec583ca3669c57586ab3f2a16f
TomasLeguizamon1207 <tomasleguizamon120708@gmail.com>
2025-07-07 19:33:12 -0500

    ultima actualizacion

--- src/models/flights.js
+++ src/models/flights.js
@@ -1,26 +1,45 @@
 import { Schema, model } from "mongoose";
 
 const flightSchema = new Schema({
-    flightNumber: {
-        type: String,
-        required: true
+  flightNumber: {
+    type: String,
+    required: true,
+    uppercase: true, 
+    validate: {
+      validator: (v) => /^EFY[A-Z0-9]{1,4}$/.test(v),
+      message: (props) =>
+        `${props.value} no es un callsign. Debe empezar con EFY y tener de 4 a 7 caracteres.`,
     },
-    origin: {
-        type: String,
-        required: true
+  },
+  origin: {
+    type: String,
+    required: true,
+    uppercase: true, 
+    validate: {
+      validator: (v) => /^[A-Z]{4}$/.test(v),
+      message: (props) =>
+        `${props.value} no es un origen válido. Debe ser un codigo ICAO con 4 letras válido.`,
     },
-    destination: {
-        type: String,
-        required: true
+  },
+  destination: {
+    type: String,
+    required: true,
+    uppercase: true, 
+    validate: {
+      validator: (v) => /^[A-Z]{4}$/.test(v),
+      message: (props) =>
+        `${props.value} no es un destino válido. Debe ser un codigo ICAO con 4 letras válido.`,
     },
-    departureTime: {
-        type: Date,
-        required: true
-    },
-    status: {
-        type: String,
-        required: true
-    }
-}, { versionKey: false, timestamps: true });
+  },
+  departureTime: {
+    type: Date,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: ["departured", "scheduled", "arrived", "delayed"],
+  },
+}, { timestamps: true });
 
 export default model("Flight", flightSchema);
